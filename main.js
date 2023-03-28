d87cb2e7f4aa6ab629fa4536a4b7aef1fff5bdd0
Andrea Bellome <andrea.bellome@gmail.com>
2023-03-28 10:55:18 +0200

    blocco 1

---
+++ main.js
@@ -0,0 +1,12 @@
+
+/* blocco 1 */
+let num1 = prompt("Enter a first number");
+let num2 = prompt("Enter a second number");
+
+if (num1 > num2){
+    console.log(num1)
+} else if(num1 < num2){
+    console.log(num2)
+} else if (num1 == num2){
+    console.log('numeri uguali')
+}
