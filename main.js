17cd7cff68766749c5f6061409870545f3ceb07c
Andrea Bellome <andrea.bellome@gmail.com>
2023-03-28 10:56:25 +0200

    .

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 /* start: blocco 1 */
-let num1 = prompt("Enter a first number");
-let num2 = prompt("Enter a second number");
+let num1 = prompt("Enter first number");
+let num2 = prompt("Enter second number");
 
 if (num1 > num2){
     console.log(num1)
 } else if(num1 < num2){
     console.log(num2)
 } else if (num1 == num2){
-    console.log('numeri uguali')
+    console.log('same numbers')
 }
 /* end: blocco 1 */
