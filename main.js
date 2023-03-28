ed7dff77b87d60abb390af8cfa51107400a8b681
Andrea Bellome <andrea.bellome@gmail.com>
2023-03-28 11:30:22 +0200

    blocco 2-3

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 /* start: blocco 1 */
-let num1 = prompt("Enter first number");
+/* let num1 = prompt("Enter first number");
 let num2 = prompt("Enter second number");
 
 if (num1 > num2){
@@ -9,5 +9,32 @@ if (num1 > num2){
     console.log(num2)
 } else if (num1 == num2){
     console.log('same numbers')
-}
+} */
 /* end: blocco 1 */
+
+/* start: blocco 2 */
+/* let word1 = prompt("Enter first word", "Hello")
+let word2 = prompt("Enter second word", "World")
+
+if (word1.length > word2.length){
+    console.log(word2, word1)
+} else if (word1.length > word2.length){
+    console.log(word1, word2)
+} else if (word1.length == word2.length){
+    console.log('same length')
+} */
+/* end: blocco 2 */
+
+/* start: blocco 3 */
+var arr = [];
+for (i=0; i <= 4; i++){
+    let num = parseFloat(prompt("Enter a number", "1"));
+    arr.push(num);
+}
+
+let result = arr.reduce((a, b) => {
+    return a + b;
+  }, 0);
+
+  console.log(result)
+  /* end: blocco 3 */
